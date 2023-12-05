b9fdc2131656c1e60f6e5a276772a9ef027b2675
FilipSvensson2023 <150144088+FilipSvensson2023@users.noreply.github.com>
2023-12-05 11:11:59 +0530

    push

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-config')(grunt);
-
+ 
     grunt.loadNpmTasks('grunt-run');
     grunt.loadNpmTasks('grunt-stylelint');
     grunt.registerTask('default', ['eslint', 'svgstore']);
